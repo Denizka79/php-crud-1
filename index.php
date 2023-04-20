9f2f5f45a4015331247195fd73306bf63a6c1b72
Мещеряков Денис Владимирович <Meshcheryakov-DV@rshbintech.ru>
2023-04-20 18:38:23 +0300

    Added sorting

--- index.php
+++ index.php
@@ -12,51 +12,63 @@ $sql_type = "SELECT type FROM products GROUP BY type";
 $result_type = mysqli_query($conn, $sql_type);
 $types = mysqli_fetch_all($result_type, MYSQLI_ASSOC);
 
-$sql_prod = "SELECT * FROM products";
 $fields = [];
-$sql_second = '';
+$sql_order = "";
 
-if ($_POST["prodtype"] != '') {
-    $prodtype_value = [" type = ", "'" . $_POST["prodtype"] .  "'"];
-    array_push($fields, $prodtype_value);
-}
+$sql_prod = "SELECT * FROM products";
 
-if ($_POST["prodvendor"] != '') {
-    $prodvendor_value = $_POST["prodvendor"];
-    array_push($fields, [" vendor = ", "'" . $prodvendor_value . "'"]);
-}
-
-if ($_POST["pricefrom"] != '') {
-    $pricefrom_value = $_POST["pricefrom"];
-    array_push($fields, [" price >= ", $pricefrom_value]);
-}
-
-if ($_POST["pricetill"] != '') {
-    $pricetill_value = $_POST["pricetill"];
-    array_push($fields, [" price <= ", $pricetill_value]);
-}
-
-if (count($fields) > 0) {
-    $sql_prod = $sql_prod . " WHERE ";
-    if (count($fields) == 1) {
-        $sql_prod = $sql_prod . $fields[0][0] . $fields[0][1];
-    } elseif (count($fields) > 1) {
-        for ($i = 0; $i < count($fields); $i++) {
-            if ($i < (count($fields) - 1)) {
-                $sql_and = " AND ";
-            } else {
-                $sql_and = "";
-            }
-            $sql_second = $sql_second . $fields[$i][0] . $fields[$i][1] . $sql_and;
-        }
-        $sql_prod = $sql_prod . $sql_second;
+if (isset($_POST["submit"])) {
+    if (isset($_POST["prodtype"]) && $_POST["prodtype"] != '') {
+        $prodtype_value = [" type = ", "'" . $_POST["prodtype"] .  "'"];
+        array_push($fields, $prodtype_value);
     }
+    
+    if (isset($_POST["prodvendor"]) && $_POST["prodvendor"] != '') {
+        $prodvendor_value = $_POST["prodvendor"];
+        array_push($fields, [" vendor = ", "'" . $prodvendor_value . "'"]);
+    }
+    
+    if (isset($_POST["pricefrom"]) && $_POST["pricefrom"] != '') {
+        $pricefrom_value = $_POST["pricefrom"];
+        array_push($fields, [" price >= ", $pricefrom_value]);
+    }
+    
+    if (isset($_POST["pricetill"]) && $_POST["pricetill"] != '') {
+        $pricetill_value = $_POST["pricetill"];
+        array_push($fields, [" price <= ", $pricetill_value]);
+    }
+
+    if (isset($_POST["sort"]) && $_POST["sort"] != '') {
+        $sql_order = " ORDER BY " . $_POST["sort"];
+    }
+    
+    if (count($fields) > 0) {
+        $sql_prod = $sql_prod . " WHERE ";
+        if (count($fields) == 1) {
+            $sql_prod = $sql_prod . $fields[0][0] . $fields[0][1];
+        } elseif (count($fields) > 1) {
+            $sql_second = "";
+            for ($i = 0; $i < count($fields); $i++) {
+                if ($i < (count($fields) - 1)) {
+                    $sql_and = " AND ";
+                } else {
+                    $sql_and = "";
+                }
+                $sql_second = $sql_second . $fields[$i][0] . $fields[$i][1] . $sql_and;
+            }
+            $sql_prod = $sql_prod . $sql_second . $sql_order;
+        }
+    }
+    $sql_prod = $sql_prod . $sql_order;
 }
 
-//echo $sql_second;
-//echo $sql_prod;
-//var_dump($fields);
-//echo count($fields);
+if (isset($_POST["clear"])) {
+    unset($_POST["prodtype"]);
+    unset($_POST["prodvendor"]);
+    unset($_POST["pricefrom"]);
+    unset($_POST["pricetill"]);
+    unset($_POST["sort"]);
+}
 
 $result_prod = mysqli_query($conn, $sql_prod);
 $products = mysqli_fetch_all($result_prod, MYSQLI_ASSOC);
@@ -86,14 +98,14 @@ $products = mysqli_fetch_all($result_prod, MYSQLI_ASSOC);
             <form class="search-filter" action="index.php" method="post">
                 <p>По производителю:</p>
                 <select name="prodvendor" id="">
-                    <option value=""></option>
+                    <option selected disabled>Выберите произовдителя</option>
                     <?php foreach($vendors as $vendor) : ?>
                     <option value="<?php echo $vendor["vendor"]; ?>"><?php echo $vendor["vendor"]; ?></option>
                     <?php endforeach; ?>
                 </select>
                 <p>По типу:</p>
                 <select name="prodtype" id="">
-                    <option value=""></option>
+                    <option selected disabled>Выберите тип</option>
                     <?php foreach($types as $type) : ?>
                     <option value="<?php echo $type["type"]; ?>"><?php echo $type["type"]; ?></option>
                     <?php endforeach; ?>
@@ -108,7 +120,17 @@ $products = mysqli_fetch_all($result_prod, MYSQLI_ASSOC);
                     <input type="number" name="pricetill">
                 </label>
                 <br>
-                <button type="submit">Искать</button>
+                <p>Сортировать:</p>
+                <select name="sort">
+                    <option selected disabled>Выберите вид сортировки</option>
+                    <option value="price ASC">По возрастанию цены</option>
+                    <option value="price DESC">По убыванию цены</option>
+                    <option value="type">По типу</option>
+                    <option value="title">По алфавиту</option>
+                </select>
+                <br>
+                <button type="submit" name="submit">Искать</button>
+                <button type="submit" name="clear">Очистить</button>
             </form>
         </div>
         <div class="items">
@@ -118,7 +140,7 @@ $products = mysqli_fetch_all($result_prod, MYSQLI_ASSOC);
                 <th>ЦЕНА</th>
                 <?php foreach($products as $prod) : ?>
                 <tr>
-                    <td><?php echo $prod["title"] ?></td>
+                    <td class="prodname"><?php echo $prod["title"] ?></td>
                     <td><?php echo $prod["description"] ?></td>
                     <td><?php echo $prod["price"] ?></td>
                 </tr>
